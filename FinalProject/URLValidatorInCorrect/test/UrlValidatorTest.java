b8b764a44ecc0c23cb34385f3a35b63273828589
lbeckman314 <lbeckman314@gmail.com>
2019-03-11 16:17:28 -0700

    Updated url validator.

--- FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -1,5 +1,3 @@
-
-
 import junit.framework.TestCase;
 
 //You can use this as a skeleton for your 3 different test approach
@@ -7,43 +5,38 @@ import junit.framework.TestCase;
 // Again, it is up to you to use this file or not!
 
 
-
-
-
 public class UrlValidatorTest extends TestCase {
 
 
-   public UrlValidatorTest(String testName) {
-      super(testName);
-   }
-
-   
-   
-   public void testManualTest()
-   {
-//You can use this function to implement your manual testing	   
-	   
-   }
-   
-   
-   public void testYourFirstPartition()
-   {
-	 //You can use this function to implement your First Partition testing	   
-
-   }
-   
-   public void testYourSecondPartition(){
-		 //You can use this function to implement your Second Partition testing	   
-
-   }
-   //You need to create more test cases for your Partitions if you need to 
-   
-   public void testIsValid()
-   {
-	   //You can use this function for programming based testing
-
-   }
-   
+	public UrlValidatorTest(String testName) {
+		super(testName);
+	}
 
 
+
+	public void testManualTest()
+	{
+		//You can use this function to implement your manual testing	   
+		UrlValidator urlChecker = new UrlValidator();
+		assertTrue(urlChecker.isValid("http://server/~foldername"));
+	}
+
+
+	public void testYourFirstPartition()
+	{
+		//You can use this function to implement your First Partition testing	   
+
+	}
+
+	public void testYourSecondPartition(){
+		//You can use this function to implement your Second Partition testing	   
+
+	}
+	//You need to create more test cases for your Partitions if you need to 
+
+	public void testIsValid()
+	{
+		//You can use this function for programming based testing
+
+	}
 }
