98b19206ac3990b2add30a017ffc7d0ade049896
lbeckman314 <lbeckman314@gmail.com>
2019-03-12 15:33:01 -0700

    Updated random testing.

--- FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -1,6 +1,9 @@
 
 
 import junit.framework.TestCase;
+import java.util.Random;
+import java.lang.Math;
+import java.nio.charset.Charset; 
 
 //You can use this as a skeleton for your 3 different test approach
 //It is an optional to use this file, you can generate your own test file(s) to test the target function!
@@ -13,37 +16,89 @@ import junit.framework.TestCase;
 public class UrlValidatorTest extends TestCase {
 
 
-   public UrlValidatorTest(String testName) {
-      super(testName);
-   }
+	public UrlValidatorTest(String testName) {
+		super(testName);
+	}
 
-   
-   
-   public void testManualTest()
-   {
-//You can use this function to implement your manual testing	   
-	   
-   }
-   
-   
-   public void testYourFirstPartition()
-   {
-	 //You can use this function to implement your First Partition testing	   
+	public void testManualTest()
+	{
+		//You can use this function to implement your manual testing	   
+		UrlValidator urlChecker = new UrlValidator();
+		assertTrue(!urlChecker.isValid("http://server/~foldername"));  
+	}
 
-   }
-   
-   public void testYourSecondPartition(){
-		 //You can use this function to implement your Second Partition testing	   
+	// scheme partition (e.g. https) 
+	public void testYourFirstPartition()
+	{
+		UrlValidator urlChecker = new UrlValidator();
+        Random random = new Random(); 	
+        int limitLeft = (int) 'a';
+        int limitRight = (int) 'z';
+        int stringLength = 10;
+        int stringNum = 100;
 
-   }
-   //You need to create more test cases for your Partitions if you need to 
-   
-   public void testIsValid()
-   {
-	   //You can use this function for programming based testing
+        for (int i = 0; i < stringNum; i++) {
+        	StringBuilder buffer = new StringBuilder(stringLength);
+        	for (int n = 0; n < stringLength; n++) {
+        		int rand = limitLeft + (int) (random.nextFloat() * (limitRight - limitLeft + 1));
+        		buffer.append((char) rand);
+        	}
+        	String finalString = buffer.toString();
+        	System.out.println(finalString);
+        }
+	}
 
-   }
-   
+	// authority partition (e.g. 192.168.107.123) 
+	public void testYourSecondPartition(){
+
+	}
+
+	// port partition (e.g. 443) 
+	public void testYourThirdPartition(){
+		// number of random tests to run
+		int RANDOM_TESTS = 100;
+		String VALID_URL = "https://coolsite.org";
+		UrlValidator urlValidator = new UrlValidator();
+
+		// limit valid ports from 0 to 65536 exclusive (2^16)
+        for (int i = 1; i < RANDOM_TESTS; i++) {
+        	
+        	// Generate random integers in range (-2 * 65536 - 1) to (2 * 65536 - 1)
+        	// This should result in one quarter of the resulting values being
+        	// valid port numbers (0 to 65536 exclusive).
+        	int leftLimit = (int) (-2 * Math.pow(2,16) - 1);
+        	int rightLimit = (int) (2 * Math.pow(2,16) - 1);
+        	int rand = leftLimit + (int) (new Random().nextFloat() * (rightLimit - leftLimit));
+
+        	// valid port
+        	if (rand >= 0 && rand < (int)(Math.pow(2, 16))) {
+				System.out.println(VALID_URL + ":" + rand); 
+				assertTrue(urlValidator.isValid(VALID_URL + ":" + rand));
+        	}
+        	
+        	// invalid port
+        	else {
+				assertFalse(urlValidator.isValid(VALID_URL + ":" + rand));
+        	}
+        }
+	}
+	
+	// path partition (e.g. /cool/new/book.html)
+	public void testYourFourthPartition(){
+
+	}
+
+	// queries partition (e.g. ?chapter=1)
+	public void testYourFifthPartition(){
+
+	}
+
+	public void testIsValid()
+	{
+		//You can use this function for programming based testing
+
+	}
+
 
 
 }
