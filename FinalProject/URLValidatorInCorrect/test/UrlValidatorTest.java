48270bc6d9678a28b6fd847da81fd0adc4aacd20
lbeckman314 <lbeckman314@gmail.com>
2019-03-12 21:27:38 -0700

    Updated scheme testing.

--- FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -2,6 +2,7 @@
 
 import junit.framework.TestCase;
 import java.util.Random;
+import java.util.Vector;
 import java.lang.Math;
 import java.nio.charset.Charset; 
 
@@ -24,27 +25,72 @@ public class UrlValidatorTest extends TestCase {
 	{
 		//You can use this function to implement your manual testing	   
 		UrlValidator urlChecker = new UrlValidator();
-		assertTrue(!urlChecker.isValid("http://server/~foldername"));  
+		//assertTrue(urlChecker.isValid("http://server/~foldername"));  
 	}
 
 	// scheme partition (e.g. https) 
+	// valid characters: [a-zA-Z0-9\-\+]
+	// invalid characters: all other characters.
+	// https://www.iana.org/assignments/uri-schemes/uri-schemes.xhtml
+	// https://tools.ietf.org/html/rfc7595
 	public void testYourFirstPartition()
 	{
 		UrlValidator urlChecker = new UrlValidator();
+		String VALID_URL = "coolsite.org";
         Random random = new Random(); 	
-        int limitLeft = (int) 'a';
-        int limitRight = (int) 'z';
-        int stringLength = 10;
+        int stringLengthMin = 0;
+        int stringLengthMax = 10;
         int stringNum = 100;
+        
+        Vector chars = new Vector();
+
+        // add all valid characters
+        for (int i = (int) 'a'; i < (int) 'z'; i++) {
+        	chars.add(i);
+        }
+        for (int i = (int) 'A'; i < (int) 'Z'; i++) {
+        	chars.add(i);
+        }
+        for (int i = (int) '0'; i < (int) '9'; i++) {
+        	chars.add(i);
+        }
+        chars.add((int) '-');
+        chars.add((int) '+');
+
+        // add some invalid characters
+        chars.add((int) ' ');
+        chars.add((int) '/');
+        chars.add((int) ':');
+
+        int limitLeft = 0;
+        int limitRight = chars.size() - 1;
 
         for (int i = 0; i < stringNum; i++) {
-        	StringBuilder buffer = new StringBuilder(stringLength);
-        	for (int n = 0; n < stringLength; n++) {
+        	int randLength = stringLengthMin + (int) (random.nextFloat() * (stringLengthMax - stringLengthMin + 1));
+        	StringBuilder buffer = new StringBuilder(randLength);
+        	for (int n = 0; n < randLength; n++) {
         		int rand = limitLeft + (int) (random.nextFloat() * (limitRight - limitLeft + 1));
-        		buffer.append((char) rand);
+        		char randChar = (char)(int)(chars.get(rand));
+        		//System.out.println(chars.get(rand));
+        		//System.out.println("test:" + test);
+        		buffer.append(randChar);
         	}
-        	String finalString = buffer.toString();
-        	System.out.println(finalString);
+        	String scheme = buffer.toString();
+        	System.out.println("scheme:" + scheme);
+
+        	// assert false for all invalid schemes
+        	if (scheme.length() == 0) {
+				assertFalse(urlChecker.isValid(scheme + "://" + VALID_URL));
+        	}
+        	if (scheme.contains(" ") || scheme.contains("/") || scheme.contains(":")) {
+				assertFalse(urlChecker.isValid(scheme + "://" + VALID_URL));
+        	}
+
+        	// assert true for all valid schemes
+        	else {
+				assertTrue(urlChecker.isValid(scheme + "://" + VALID_URL));
+        	}
+        	
         }
 	}
 
